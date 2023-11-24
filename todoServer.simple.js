8ccf0f8eb49b9ee596831f4b20612adb7345ecc5
adityabarodiya <adi25kht@gmail.com>
2023-11-24 13:26:43 +0530

    add edit button to update todo

--- todoServer.simple.js
+++ todoServer.simple.js
@@ -72,15 +72,12 @@ app.delete("/todos/:id", (req, res) => {
 app.get('/',(req,res) =>{
     res.sendFile(path.join(__dirname,"index.html"))
 })
-// for all other routes, return 404
-// app.use((req, res, next) => {
-//   res.status(404).send();
-// });
 
-
-
-app.listen(port, ()=>{
-  console.log(`Listining on port ${port} simple `);
+app.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname,"style.css"))
 })
 
+app.listen(port, () => {
+  console.log(`Listining on port ${port} simple `);
+})
 module.exports = app;
